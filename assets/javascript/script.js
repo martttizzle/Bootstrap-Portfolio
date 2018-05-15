12ed57263aa2a3f9b5e5c5eaaccdd05ae6d40ab7
Vinod <ramdinv@gmail.com>
2018-05-15 12:58:48 -0500

    added in parallax and fixed background

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,13 @@
- 
+$(window).scroll(function() {
+    parallax();
+});
+
+function parallax() {
+    var wScroll = $(window).scrollTop();
+    $(".parallax--bg").css('background-position', 
+'center ' +(wScroll*0.55) +'px');
+    $('.parallax--box').css('top', 
+    -5+(wScroll*0.01101)+'em');
+
+
+}
