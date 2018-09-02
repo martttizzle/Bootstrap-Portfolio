09743ee518c7761136c13e55c67757b6181d0c7c
Vinod <martel1983@yahoo.com>
2018-09-02 13:31:59 -0500

    almost done, need to add in profile words and the projects

--- assets/javascript/navbar.js
+++ assets/javascript/navbar.js
@@ -1,13 +1,35 @@
-// $(window).scroll(function() {
-//    if($(document).scrollTop() > 50) {
-//        $("nav").addClass("shrink");
-//    }
-// else{
-//     $('nav').removeClass('shrink');
-// }
+$(window).scroll(function() {
+   if($(document).scrollTop() > 750) {
+   $('#fixedTop').attr('class','fixed-top container-fluid naver')
+   $('#navLinks').addClass("anchorTags");
+//    alert("helo");
+   }
+else{
+    // $('nav').removeClass('naver');
+}
 
-// });
+
  
-$("#seeWorkBtn").on("click", function(){
-    alert("Hello");
+// $("#seeWorkBtn").on("click", function(){
+//     alert("Hello");
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
