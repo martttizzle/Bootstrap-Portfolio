eaedd7389f0f44bfa87d96c19afce94534f06fb7
Vinod <martel1983@yahoo.com>
2018-09-03 18:24:53 -0500

    added minor changes

--- assets/javascript/navbar.js
+++ assets/javascript/navbar.js
@@ -1,35 +1,29 @@
+
+ 
+
 $(window).scroll(function() {
    if($(document).scrollTop() > 750) {
    $('#fixedTop').attr('class','fixed-top container-fluid naver')
    $('#navLinks').addClass("anchorTags");
-//    alert("helo");
+
    }
 else{
-    // $('nav').removeClass('naver');
-}
-
-
- 
-// $("#seeWorkBtn").on("click", function(){
-//     alert("Hello");
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  
+ }
 
 });
+
+
+
+$("#seeWorkBtn").scroll("click", function(){
+    $(document).scrollTop() = 750;
+});
+
+
+
+
+
+
+
+
+
