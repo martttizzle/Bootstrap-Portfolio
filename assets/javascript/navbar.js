25c360f6df9b85caab71f81c67998320d8378c15
Vinod <martel1983@yahoo.com>
2018-09-05 18:55:58 -0500

    fixed project btn size, background color of body card, changed body card

--- assets/javascript/navbar.js
+++ assets/javascript/navbar.js
@@ -1,29 +1,50 @@
+$(document).ready(function() {
+
+
+//Nav Scroll    
+
+// ^= means hrefs target any a that have #1
+$('a[href^="#1"]').on('click', function(e) {
+e.preventDefault();
+
+var target = this.hash;
+var $target = $(target);
+
+$('html, body').animate({
+    'scrollTop': $target.offset().top 
+}, 1000, 'swing', function () {
+    window.location.hash = target;
+ });
+});
+
+$('#seeWorkBtn').on('click', function(e){
+
+    e.preventDefault();
+    var buttonTarget = this.seeWorkBtn; 
+    var $buttonTarget = $('#1abtsm');
+
+    $('html, body').animate({
+        'scrollTop': $buttonTarget.offset().top 
+    }, 1000, 'swing', function () {
+        window.location.hash = $buttonTarget;
+     });
+    });
 
  
 
+
+// Nav Bar sticky
 $(window).scroll(function() {
-   if($(document).scrollTop() > 750) {
+   if($(document).scrollTop() > 760) {
    $('#fixedTop').attr('class','fixed-top container-fluid naver')
    $('#navLinks').addClass("anchorTags");
-
    }
 else{
-  
  }
-
-});
-
-
-
-$("#seeWorkBtn").scroll("click", function(){
-    $(document).scrollTop() = 750;
 });
 
 
 
 
-
-
-
-
-
+    
+});
